72b0e0d8d694c65017340fa5ac3867dbfbc35aa8
Bobby <bhackett@uw.edu>
2023-11-22 09:08:58 -0700

    my snapshot

---
+++ cat.py
@@ -0,0 +1,16 @@
+def main():
+    number = get_number()
+    meow(number)
+
+def get_number():
+    while True:
+        n = int(input("How many meows?"))
+        if n > 0:
+            return n
+
+def meow(n):
+    for _ in range(n):
+        print("meow")
+
+
+main()
