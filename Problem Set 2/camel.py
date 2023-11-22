4b4c9961f6b8d883c8ed992b3697fa5f65f22789
Bobby <bhackett@uw.edu>
2023-11-22 13:07:29 -0700

    Completed work from problem set 2

---
+++ Problem Set 2/camel.py
@@ -0,0 +1,11 @@
+# In a file called camel.py, implement a program that prompts the user for the name of a variable in camel case 
+# and outputs the corresponding name in snake case. 
+# Assume that the user’s input will indeed be in camel case.
+
+text = input("write in camelCase: ")
+
+for i in text:
+    if i.isupper():
+       text = text.replace(i, "_"+i.lower())
+
+print(text)
---
+++ Problem Set 2/plates.py
@@ -0,0 +1,33 @@
+# In plates.py, implement a program that prompts the user for a vanity plate and 
+# then output Valid if meets all of the requirements or Invalid if it does not. 
+# Assume that any letters in the user’s input will be uppercase. 
+# Structure your program per the below, wherein is_valid returns True if s meets all requirements and False if it does not. 
+# Assume that s will be a str. 
+# You’re welcome to implement additional functions for is_valid to call (e.g., one function per requirement).
+
+# “All vanity plates must start with at least two letters.”
+# “… vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters.”
+# “Numbers cannot be used in the middle of a plate; they must come at the end. For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable. 
+# The first number used cannot be a ‘0’.”
+# “No periods, spaces, or punctuation marks are allowed.”
+
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+def is_valid(s):
+    pattern = r"^[a-zA-Z]{2}[a-zA-Z1-9]{0,4}((?<=[1-9])[0-9]){0,4}$"
+    if 2 < len(s) > 6:
+        return False
+    else:
+        if re.match(pattern, s):
+            return True
+        else:
+            return False
+
+import re
+
+main()
