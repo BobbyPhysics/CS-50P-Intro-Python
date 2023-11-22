b6c5969d0211a0671c46fce5981583e63b7d2bfd
Bobby <bhackett@uw.edu>
2023-11-22 13:14:59 -0700

    Updated problem instructions

--- Problem Set 2/nutrition.py
+++ Problem Set 2/nutrition.py
@@ -1,3 +1,8 @@
+# In a file called nutrition.py, implement a program that prompts consumers users to input a fruit (case-insensitively) and 
+# then outputs the number of calories in one portion of that fruit, per the FDA’s poster for fruits, which is also available as text. 
+# Capitalization aside, assume that users will input fruits exactly as written in the poster (e.g., strawberries, not strawberry). 
+# Ignore any input that isn’t a fruit.
+
 fruits = [
     {"fruit":"apple", "calories":130},
     {"fruit":"avocado", "calories":50},
