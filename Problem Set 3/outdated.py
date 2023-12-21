b7ee0f58ddc2aed7f0f430074d0e24d9f40ce7dd
Bobby <bhackett@uw.edu>
2023-12-21 08:43:44 -0700

    Completed Outdated problem

---
+++ Problem Set 3/outdated.py
@@ -0,0 +1,45 @@
+months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+def main():
+    format_date()
+
+def format_date():
+    while True:
+        # get user input in form mm/dd/yyyy or month dd, yyyy
+        date = input("Date: ")
+        try:
+            # for the first case, split on /
+            month, day, year = date.split("/")
+            if 1 <= int(month) <= 12 and 1<= int(day) <= 31:
+                break
+        # nesting another "try" for the second case
+        except:
+            try:
+                # for the second case, remove comma and split on spaces
+                date = date.replace(",", "")
+                string_month, day, year = date.split(" ")
+                string_month = string_month.capitalize()
+                # compare string_month to the list of months
+                for i in range(len(months)):
+                    if string_month == months[i]:
+                        month = i + 1
+                if 1<= int(month) <= 12 and 1 <= int(day) <= 31:
+                    break
+            except:
+                break
+    print(f"{year}-{int(month):02}-{int(day):02}")
+
+main()
